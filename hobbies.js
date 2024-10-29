30fb49ab777d04d2cd06dca4e0744914f252a63f
Gargenziano03 <gargenziano03@gmail.com>
2024-10-29 14:54:55 +0100

    initial

---
+++ hobbies.js
@@ -0,0 +1,4 @@
+/*2 
+Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. 
+La funzione dovrebbe restituire un oggetto con una proprietà hobbies, che è un array di hobby.
+Esporta la funzione dal file.*/
