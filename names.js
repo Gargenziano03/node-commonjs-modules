f3b32ca669f6b024daa827abf413fb294bd0346a
Gargenziano03 <gargenziano03@gmail.com>
2024-10-29 17:06:42 +0100

    add function creahobbies

--- names.js
+++ names.js
@@ -3,12 +3,16 @@ Crea una funzione che accetta due parametri: firstName, lastName.
 La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 Esporta la funzione dal file.*/
 
+//definiamo
+
 function names(firstName, lastName) {
     return{
-        firstName: 'Cristiano',
-        lastName: 'Ronaldo'
-    }
+        firstName: firstName,
+        lastName: lastName
+    };
 }
 
-module.exports = names()
+//esportazione
+module.exports = names
+
 
--- people.js
+++ people.js
@@ -4,7 +4,7 @@ Importa la tua funzione da hobbies.js
 Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies.
 All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.*/
 
-const utente = require('./names.js')
-const names = utente.names(firstName, lastName)
-console.log(names);
+//importazione
+const names = require('./names.js')
+names('cristiano', 'ronaldo')
  
